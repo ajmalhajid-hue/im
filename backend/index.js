bc02bb3d6460c2de237f687e1d225d9ab59ccbd6
ajmalhajid-hue <ajmalhajid@gmail.com>
2026-06-25 10:01:43 +0530

    First commit

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+const app =express();
 
 /* Home Route */
 app.get("/prethiv", (req, res) => {
